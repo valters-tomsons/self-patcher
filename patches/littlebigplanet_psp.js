537923adec2b83e77ac89fa944356c4ba34a546a
Valters Tomsons <valters.tomsons@protonmail.com>
2025-12-09 20:08:47 +0200

    Delete LBP patch files

--- patches/littlebigplanet_psp.js
+++
@@ -1,8 +0,0 @@
-//to be applied over base_littlebigplanet - this is the base argument
-
-return base
-	.replace(/GAME_NAME/g, "LittleBigPlanet PSP")
-	.replace(/GAME_URLS/g, `{
-			http: new BinaryUrl("http://%s:10060/LITTLEBIGPLANETPSP_XML%s"),
-			https: new BinaryUrl("https://%s:10061/LITTLEBIGPLANETPSP_XML%s")
-		}`);
